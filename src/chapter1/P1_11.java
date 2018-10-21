4f5eb756250b24c3242200cc83b09bd50a7b8a1b
zhangzhen <zz409119@126.com>
2018-10-22 01:44:44 +0800

    use underline in large number

--- src/chapter1/P1_11.java
+++ src/chapter1/P1_11.java
@@ -2,7 +2,7 @@ package chapter1;
 
 public class P1_11 {
 	public static void main(String[] args) {
-		long currentPopulation = 312032486;
+		long currentPopulation = 312_032_486;	// Use underline in large number
 		int secondsPerYear = 60 * 60 * 24 * 365;
 		int bornPerYear = (int)(secondsPerYear / 7.0);
 		int deathPerYear = (int)(secondsPerYear / 13.0);
