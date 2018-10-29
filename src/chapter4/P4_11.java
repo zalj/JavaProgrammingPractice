4212f49c6b718a2e78c1cafdb6023e2dbacef78a
zhangzhen <zz409119@126.com>
2018-10-29 15:45:11 +0800

    4-11

--- src/chapter4/P4_11.java
+++ src/chapter4/P4_11.java
@@ -1,18 +1,27 @@
 package chapter4;
 
+import java.util.Scanner;
 
 public class P4_11 {
 	
 	public static void main(String[] args) {
-		Object object = new Student(1, "James");
-		System.out.println(object.getClass());
-		Student o = (Student)object;
-		System.out.println(o.getId());
+		Scanner input = new Scanner(System.in);
+		while(true) {
+			System.out.print("Enter: ");
+			String token = input.nextLine();
+			if(token.equals("EOF"))
+				break;
+			System.out.print(token + " is ");
+			if(!isNumeric(token))
+				System.out.print("not ");
+			System.out.println("a numberic.");
+		}
 	}
-	static class Student{
+	
+	static class Person{
 		private int id;
 		private String name;
-		public Student(int id, String name) {
+		public Person(int id, String name) {
 			this.id = id;
 			this.name = name;
 		}
@@ -23,6 +32,39 @@ public class P4_11 {
 		
 		public int getId() {
 			return this.id;
+		}
+		
+		public Person() {
+			
+		}
+	}
+	static class Student extends Person{
+		private int grade;
+		
+		public Student(int grade) {
+			this.grade = grade;
+		}
+	}
+
+	public static boolean isNumeric(String token) {
+		token = token.trim();
+		if(token.equals(""))
+			return false;
+		if(token.matches("[0-9]*[.]?[0-9]*"))
+			return true;
+		return false;
+	}
+}
+
+class Circle{
+	double radius;
+	
+	@Override
+	public boolean equals(Object o) {
+		if(o instanceof Circle) {
+			return this.radius == ((Circle)o).radius;
+		}else {
+			return this == o;
 		}
 	}
 }
